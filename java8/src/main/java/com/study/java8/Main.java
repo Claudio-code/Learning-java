874fa2467303a17c6baa5aebdb7d557211689512
claudio-code <suporte2424@outlook.com>
2021-09-13 17:32:16 -0300

    changed exercises of lambda functions for new class

--- java8/src/main/java/com/study/java8/Main.java
+++ java8/src/main/java/com/study/java8/Main.java
@@ -1,24 +1,8 @@
 package com.study.java8;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        Runnable run = new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("java 8 function");
-            }
-        };
-        new Thread(run).start();
-
-        Runnable runnable = () -> System.out.println("java 8 lambda parameter");
-        new Thread(runnable).start();
-
-        new Thread(() -> System.out.println("java 8 lambda")).start();
-
-        List<String> stings = Arrays.asList("there", "how", "new job");
-        stings.forEach(str -> System.out.println(str));
+        LambdasStuding lambda = new LambdasStuding();
+        lambda.execute();
     }
 }
