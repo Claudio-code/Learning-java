9a38f3500623484ab0d4839951110fa121b4c4ca
claudio-code <suporte2424@outlook.com>
2021-09-15 18:14:35 -0300

    added example how uses Optional to scape from Null Pointer exceptions

--- java8/src/main/java/com/study/java8/Main.java
+++ java8/src/main/java/com/study/java8/Main.java
@@ -1,8 +1,22 @@
 package com.study.java8;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
         LambdasStuding lambda = new LambdasStuding();
         lambda.execute();
+
+        List<Optional<Person>> peoples = new ArrayList<>();
+        Optional<Person> person1 = Optional.of(new Person("Antonia"));
+        Optional<Person> person2 = Optional.of(new Person(null));
+        Optional<Person> person3 = Optional.of(new Person("mario"));
+        peoples.add(person1);
+        peoples.add(person2);
+        peoples.add(person3);
+
+        peoples.forEach(op -> op.ifPresent(p -> System.out.println(p.getName())));
     }
 }
